445c7dacc2ff0fd2e02b2ff16748cda46a1a88c4
Matthew Kaufer <mjkaufer@gmail.com>
2017-09-08 11:24:27 -0400

    Move qr.js to head instead of body

--- client/js/qr.ts
+++ client/js/qr.ts
@@ -1,17 +1,23 @@
-let typeNumber = 0;
-let errorCorrectionLevel = 'H';
 declare var qrcode: any;
-let qr = qrcode(typeNumber, errorCorrectionLevel);
-let qrElement = document.getElementById('qrCode') as HTMLElement;
 
-qr.addData(qrElement.attributes.getNamedItem('data').value);
-qr.make();
-// Let qrImage = qr.createImgTag() as HTMLImageElement;
-let qrImageString: string = qr.createImgTag();
+window.onload = () => {
 
-qrElement.innerHTML = qrImageString;
+	let typeNumber = 0;
+	let errorCorrectionLevel = 'H';
 
-let qrImageObject = qrElement.firstChild as HTMLImageElement;
+	let qr = qrcode(typeNumber, errorCorrectionLevel);
+	let qrElement = document.getElementById('qrCode') as HTMLElement;
 
-qrImageObject.removeAttribute('height');
-qrImageObject.removeAttribute('width');
+	qr.addData(qrElement.attributes.getNamedItem('data').value);
+	qr.make();
+
+	let qrImageString: string = qr.createImgTag();
+
+	qrElement.innerHTML = qrImageString;
+
+	let qrImageObject = qrElement.firstChild as HTMLImageElement;
+
+	qrImageObject.removeAttribute('height');
+	qrImageObject.removeAttribute('width');
+
+};
