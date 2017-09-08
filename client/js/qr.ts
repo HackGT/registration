7fde1a73e0e105782888ff0365b7d2bd5e5e64b4
Matthew Kaufer <mjkaufer@gmail.com>
2017-09-08 10:48:27 -0400

    Bigger QR code

--- client/js/qr.ts
+++ client/js/qr.ts
@@ -6,4 +6,12 @@ let qrElement = document.getElementById('qrCode') as HTMLElement;
 
 qr.addData(qrElement.attributes.getNamedItem('data').value);
 qr.make();
-qrElement.innerHTML = qr.createImgTag();
+// Let qrImage = qr.createImgTag() as HTMLImageElement;
+let qrImageString: string = qr.createImgTag();
+
+qrElement.innerHTML = qrImageString;
+
+let qrImageObject = qrElement.firstChild as HTMLImageElement;
+
+qrImageObject.removeAttribute('height');
+qrImageObject.removeAttribute('width');
