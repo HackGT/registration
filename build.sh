59e0af3aecde314e5f67078d1e060d9d87fd0403
Michael Eden <themichaeleden@gmail.com>
2017-09-19 14:21:00 -0400

    Moved build script into file.

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+SOURCE_DIR=$(readlink -f "${BASH_SOURCE[0]}")
+cd "$(dirname "$SOURCE_DIR")" || exit
+set -xeuo pipefail
+
+node node_modules/json-schema-to-typescript/dist/src/cli.js \
+     server/config/questions.schema.json \
+     > server/config/questions.schema.d.ts
+
+./node_modules/tslint/bin/tslint -p server/
+./node_modules/tslint/bin/tslint -p client/
+./node_modules/typescript/bin/tsc -p server/
+./node_modules/typescript/bin/tsc -p client/
+
