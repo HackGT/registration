2eb9274cb27426193e4a9fd320bfffb04ea2a4ac
Ryan Petschek <petschekr@gmail.com>
2018-03-03 15:38:45 -0500

    Add a second admin settings save button at the top of the page

--- client/js/admin.ts
+++ client/js/admin.ts
@@ -647,14 +647,23 @@ function parseDateTime(dateTime: string) {
 	let digits = dateTime.split(/\D+/).map(num => parseInt(num, 10));
 	return new Date(digits[0], digits[1] - 1, digits[2], digits[3], digits[4], digits[5] || 0, digits[6] || 0);
 }
-let settingsUpdateButton = document.querySelector("#settings input[type=submit]") as HTMLInputElement;
+let settingsUpdateButtons = document.querySelectorAll("#settings input[type=submit]") as NodeListOf<HTMLInputElement>;
 let settingsForm = document.querySelector("#settings form") as HTMLFormElement;
-settingsUpdateButton.addEventListener("click", e => {
+for (let i = 0; i < settingsUpdateButtons.length; i++) {
+	settingsUpdateButtons[i].addEventListener("click", settingsUpdate);
+}
+function settingsUpdateButtonDisabled(disabled: boolean) {
+	for (let i = 0; i < settingsUpdateButtons.length; i++) {
+		settingsUpdateButtons[i].disabled = disabled;
+	}
+}
+
+function settingsUpdate(e: MouseEvent) {
 	if (!settingsForm.checkValidity() || !settingsForm.dataset.action) {
 		return;
 	}
 	e.preventDefault();
-	settingsUpdateButton.disabled = true;
+	settingsUpdateButtonDisabled(true);
 
 	let teamsEnabledData = new FormData();
 	teamsEnabledData.append("enabled", (document.getElementById("teams-enabled") as HTMLInputElement).checked ? "true" : "false");
@@ -742,9 +751,9 @@ settingsUpdateButton.addEventListener("click", e => {
 		window.location.reload();
 	}).catch(async (err: Error) => {
 		await sweetAlert("Oh no!", err.message, "error");
-		settingsUpdateButton.disabled = false;
+		settingsUpdateButtonDisabled(false);
 	});
-});
+}
 
 //
 // Graphs
