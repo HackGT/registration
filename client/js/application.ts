712784c2b22e9044f5523b5a8b76f14006df4043
Ryan Petschek <petschekr@gmail.com>
2018-06-05 16:45:29 -0400

    Replace broken global variable hacks with data attributes
    Fixes missing space in formatted application submission success message and removes "We look forward to seeing you" because the message is also shown for rejected applicants.

--- client/js/application.ts
+++ client/js/application.ts
@@ -2,10 +2,9 @@ enum FormType {
 	Application,
 	Confirmation
 }
-declare let formTypeString: keyof typeof FormType;
-let formType = FormType[formTypeString];
-
-let unauthenticated: (boolean | null) = null;
+const formTypeString = document.body.dataset.formType as keyof typeof FormType;
+const formType = FormType[formTypeString];
+const unauthenticated = document.body.dataset.unauthenticated === "true";
 
 let form = document.querySelector("form") as HTMLFormElement | null;
 let submitButton = document.querySelector("form input[type=submit]") as HTMLInputElement;
@@ -21,9 +20,10 @@ submitButton.addEventListener("click", e => {
 		method: "POST",
 		body: new FormData(form)
 	}).then(checkStatus).then(parseJSON).then(async () => {
-		let successMessage: string = formType === FormType.Application ?
-			"Your application has been saved." + (!unauthenticated ? "Feel free to come back here and edit it at any time." : "") :
-			"Your RSVP has been saved. Feel free to come back here and edit it at any time. We look forward to seeing you!";
+		let successMessage: string = formType === FormType.Application ? "Your application has been saved." : "Your RSVP has been saved.";
+		if (!unauthenticated) {
+			successMessage += " Feel free to come back here and edit it at any time.";
+		}
 
 		await sweetAlert("Awesome!", successMessage, "success");
 
