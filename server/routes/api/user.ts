c5a04cc813e3d84aa3f46da7dcfd81e0f9dcaffd
Ehsan Asdar <ehsanmasdar@gmail.com>
2019-04-08 22:55:01 -0400

    Debug email code

--- server/routes/api/user.ts
+++ server/routes/api/user.ts
@@ -405,6 +405,7 @@ userRoutes.route("/send_acceptances").post(isAdmin, async (request, response): P
 			let html = await renderEmailHTML(emailMarkdown, user);
 			let text = await renderEmailText(html, user, true);
 			if (user.applicationBranch === 'Participant') {
+				console.log(user);
 				let question = user.applicationData.find(data => data.name === "parent-email")!;
 				emails.push({
 					from: config.email.from,
