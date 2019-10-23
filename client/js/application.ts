e2768f0a65d7c741d9906f4cf9da9e2c7b1461f0
Ehsan Asdar <ehsanmasdar@gmail.com>
2019-10-23 15:05:15 -0400

    Add QR Code when a walk-up user submits an application

--- client/js/application.ts
+++ client/js/application.ts
@@ -19,13 +19,22 @@ submitButton.addEventListener("click", e => {
 		credentials: "same-origin",
 		method: "POST",
 		body: new FormData(form)
-	}).then(checkStatus).then(parseJSON).then(async () => {
+	}).then(checkStatus).then(parseJSON).then(async (json) => {
 		let successMessage: string = formType === FormType.Application ? "Your application has been saved." : "Your RSVP has been saved.";
 		if (!unauthenticated) {
 			successMessage += " Feel free to come back here and edit it at any time.";
-		}
+			await sweetAlert("Awesome!", successMessage, "success");
+		} else {
+			let qr = qrcode(0, "H");
+			qr.addData(json.uuid);
+			qr.make();
 
-		await sweetAlert("Awesome!", successMessage, "success");
+			await sweetAlert({
+				type: 'success',
+				title: 'Awesome!',
+				html: `Scan this code to create badge: <br>${qr.createImgTag(8,4)}`
+			});
+		}
 
 		if (unauthenticated) {
 			document.querySelector("form")!.reset();
