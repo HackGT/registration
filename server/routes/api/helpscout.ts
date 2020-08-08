e03bcfdcc0ebb1f74c14e615abe4a6b8ab4ede80
Evan Strat <evan10s@users.noreply.github.com>
2020-08-08 16:31:33 -0400

    Testing some more

--- server/routes/api/helpscout.ts
+++ server/routes/api/helpscout.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import {isHelpScoutIntegrationEnabled, validateHelpScoutSignature} from "../../middleware";
 import {IFormItem, IUser, User} from "../../schema";
 import bodyParser = require("body-parser");
+import * as moment from "moment-timezone";
 
 export const helpScoutRoutes = express.Router({"mergeParams": true});
 
@@ -28,7 +29,7 @@ async function findUserByEmail(email: string) {
 
 function findApplicationQuestion(applicationData: IFormItem[], questionName: string) {
 	const result = applicationData.find((item: IFormItem) => item.name === questionName);
-	return result ? result : "";
+	return result ? result.value : "";
 }
 
 async function helpScoutUserInfoHandler(request: express.Request, response: express.Response) {
@@ -43,6 +44,19 @@ async function helpScoutUserInfoHandler(request: express.Request, response: expr
 				<h4>There are no users in registration with this email</h4>`
 		});
 	} else {
+		const affiliationName = user.applicationBranch === "Mentor" ? "Affiliation" : "University";
+
+		const affiliationBlock = `<li class="c-sb-list-item">
+				<span class="c-sb-list-item__label">${affiliationName}<span class="c-sb-list-item__text">
+					${user.applicationData ? findApplicationQuestion(user.applicationData, affiliationName.toLowerCase()): ""}</span>
+					</span>
+			</li>`;
+
+		const cellphoneBlock = `<li class="c-sb-list-item">
+			<span class="c-sb-list-item__label">Cell Phone<span class="c-sb-list-item__text">
+				${user.applicationData ? findApplicationQuestion(user.applicationData, "phone-number") : ""}</span>
+				</span>
+			</li>`;
 		response.status(200).json({
 			"html": `
 				<h4>
@@ -50,22 +64,16 @@ async function helpScoutUserInfoHandler(request: express.Request, response: expr
 				</h4>
 				<h4>${user.applicationBranch || "Has not applied"}</h4>
 				<ul class="c-sb-list c-sb-list--two-line">
+					${user.applicationData ? affiliationBlock : ""}
+					${user.applicationData ? cellphoneBlock : ""}
 					<li class="c-sb-list-item">
-						<span class="c-sb-list-item__label">University<span class="c-sb-list-item__text">
-							${user.applicationData ? findApplicationQuestion(user.applicationData, "university"): ""}</span>
+						<span class="c-sb-list-item__label">Applied?<span class="c-sb-list-item__text">
+						${user.applicationSubmitTime ? moment(user.applicationSubmitTime)
+								.format("DD-MMM-YYYY h:mm a") : "No application"}</span>
 						</span>
 					</li>
 					<li class="c-sb-list-item">
-						<span class="c-sb-list-item__label">Cell Phone<span class="c-sb-list-item__text">
-							${user.applicationData ? findApplicationQuestion(user.applicationData, "phone-number"): ""}</span>
-						</span>
-					</li>
-					<li class="c-sb-list-item">
-						<span class="c-sb-list-item__label">Applied?<span class="c-sb-list-item__text">${user.applicationSubmitTime}</span>
-						</span>
-					</li>
-					<li class="c-sb-list-item">
-						<span class="c-sb-list-item__label">Accepted?<span class="c-sb-list-item__text">${user.accepted ? "Yes" : "No"}</span>
+						<span class="c-sb-list-item__label">Accepted?<span class="c-sb-list-item__text">${user.accepted ? "<span class=\"badge success\">Accepted</span>" : "No"}</span>
 						</span>
 					</li>
 					<li class="c-sb-list-item">
